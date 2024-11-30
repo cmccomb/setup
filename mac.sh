f0e5b9865be58ad0ba1e984019f8b9626774340c
Chris McComb <ccmcc2012@gmail.com>
2024-11-29 22:25:43 -0500

    Added sudo and setup

--- mac.sh
+++ mac.sh
@@ -1,6 +1,21 @@
-########################################################
-################## Update Xcode Stuff ##################
-########################################################
+#!/usr/bin/env bash
+
+###############################################################################
+############################ Mise en place ####################################
+###############################################################################
+
+# Close any open System Preferences panes, to prevent them from overriding settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###############################################################################
+########################### Update Xcode Stuff ################################
+###############################################################################
 
 # Install xcode things
 sudo xcodebuild -license
