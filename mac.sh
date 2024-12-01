4a719f7e0871e6e84c15d14863de13604a3ac88a
Chris McComb <ccmcc2012@gmail.com>
2024-11-30 21:15:00 -0500

    Removing stuff that I genuinely don't care about

--- mac.sh
+++ mac.sh
@@ -195,22 +195,11 @@ sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bo
 ################################ Screen #######################################
 ###############################################################################
 
-## Require password immediately after sleep or screen saver begins
-#defaults write com.apple.screensaver askForPassword -int 1
-#defaults write com.apple.screensaver askForPasswordDelay -int 0
-#
-## Save screenshots to the desktop
-#defaults write com.apple.screencapture location -string "${HOME}/Desktop"
-#
-## Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-#defaults write com.apple.screencapture type -string "png"
-#
-## Enable subpixel font rendering on non-Apple LCDs
-## Reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
-#defaults write NSGlobalDomain AppleFontSmoothing -int 1
-#
-## Enable HiDPI display modes (requires restart)
-#sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
 
 
 ###############################################################################
