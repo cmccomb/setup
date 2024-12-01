3960a7b4784d93071c349d75f547d6b5fc4a908a
Chris McComb <ccmcc2012@gmail.com>
2024-11-30 21:15:09 -0500

    Uncommented and tested

--- mac.sh
+++ mac.sh
@@ -206,24 +206,24 @@ defaults write com.apple.screencapture type -string "png"
 ################################# Finder ######################################
 ###############################################################################
 
-## Set Desktop as the default location for new Finder windows
-## For other paths, use `PfLo` and `file:///full/path/here/`
-#defaults write com.apple.finder NewWindowTarget -string "PfDe"
-#defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
-#
-## Finder: show hidden files by default
-#defaults write com.apple.finder AppleShowAllFiles -bool true
-#
-## Finder: show path bar
-#defaults write com.apple.finder ShowPathbar -bool true
-#
-## Disable the warning when changing a file extension
-#defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-#
-## Avoid creating .DS_Store files on network or USB volumes
-#defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-#defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
-#
+# Set Desktop as the default location for new Finder windows
+# For other paths, use `PfLo` and `file:///full/path/here/`
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
 ### TODO: Show item info near icons on the desktop and in other icon views
 ##/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
 ##/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
